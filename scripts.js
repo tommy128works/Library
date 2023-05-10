4c33f979f591d74cda921327792d5a256294732b
Tommy <tommy128works@gmail.com>
2023-05-10 09:58:27 -0400

    feat: add Book constructor
    Created cook constructor and tested output using console.log().

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    if (read === true) {
+      return title + " by " + author + ", " + pages + " pages, read already";
+    } else {
+      return title + " by " + author + ", " + pages + " pages, not read yet";
+    }
+  };
+}
+
+const book1 = new Book("Spongebob Squarepants", "ME MARIO", "120", false);
+
+console.log(book1.info());
