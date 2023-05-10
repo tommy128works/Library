9586d50d95393100eb9287995161af8fb8a413a4
Tommy <tommy128works@gmail.com>
2023-05-10 16:35:34 -0400

    feat: add skeleton code of table DOM manipulation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,13 @@
 module.exports = {
-    "env": {
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "prettier"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-    ],
-    "rules": {
-    }
-}
+  env: {
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [],
+  rules: {},
+};
--- scripts.js
+++ scripts.js
@@ -18,6 +18,22 @@ function addBookToLibrary(library, book) {
   library.push(book);
 }
 
+var table = document.getElementById("myLibrary");
+
+var row = table.insertRow();
+
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+var cell3 = row.insertCell(2);
+var cell4 = row.insertCell(3);
+
+cell1.outerHTML = "<th>NEW CELL1</th>";
+cell2.textContent = "NEW CELL2";
+cell3.textContent = "NEW CELL3";
+cell4.textContent = "NEW CELL4";
+
 const book1 = new Book("Spongebob Squarepants", "ME MARIO", "120", false);
+const book2 = new Book("Spongebob Squarepants2", "ME MARIO", "120", false);
+const book3 = new Book("Spongebob Squarepants3", "ME MARIO", "120", false);
 
 console.log(book1.info());
