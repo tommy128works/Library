405cc9cf88a9ba9e04238e8e64552abcbab79578
Tommy <tommy128works@gmail.com>
2023-05-09 22:42:25 -0400

    chore: set up ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "prettier"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+    ],
+    "rules": {
+    }
+}
