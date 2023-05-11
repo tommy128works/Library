3e67987b9889fc9cd6e36bef5e92d41602fbdd83
Tommy <tommy128works@gmail.com>
2023-05-11 08:09:36 -0400

    feat: add function to display library as table
    Added displayLibrary() function that resets the table DOM and displays
    every book in the library.

--- scripts.js
+++ scripts.js
@@ -18,22 +18,38 @@ function addBookToLibrary(library, book) {
   library.push(book);
 }
 
-var table = document.getElementById("myLibrary");
+function displayLibrary(library) {
+  var table = document.getElementById("myLibrary");
+  var numRows = document.getElementById("myLibrary").rows.length;
+  for (let i = 0; i < numRows - 1; i++) {
+    table.deleteRow(1);
+  }
 
-var row = table.insertRow();
+  for (let i = 0; i < library.length; i++) {
+    var row = table.insertRow();
 
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-var cell3 = row.insertCell(2);
-var cell4 = row.insertCell(3);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
 
-cell1.outerHTML = "<th>NEW CELL1</th>";
-cell2.textContent = "NEW CELL2";
-cell3.textContent = "NEW CELL3";
-cell4.textContent = "NEW CELL4";
+    cell1.outerHTML = `<th>${i + 1}</th>`;
+    cell2.textContent = library[i].title;
+    cell3.textContent = library[i].author;
+    cell4.textContent = library[i].pages;
+    cell5.textContent = library[i].read;
+    cell6.textContent = library[i].read;
+  }
+}
 
 const book1 = new Book("Spongebob Squarepants", "ME MARIO", "120", false);
 const book2 = new Book("Spongebob Squarepants2", "ME MARIO", "120", false);
 const book3 = new Book("Spongebob Squarepants3", "ME MARIO", "120", false);
 
-console.log(book1.info());
+addBookToLibrary(myLibrary, book1);
+addBookToLibrary(myLibrary, book2);
+addBookToLibrary(myLibrary, book3);
+
+displayLibrary(myLibrary);
